c2c5e5055d2a6c681a5c4a28bb551f73b0078f4c
gonzalo-novak <gonzalo.novak@monstertrap.art>
2021-09-24 11:32:16 -0500

    chore(db): staging configuration

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
+	if(process.env.NODE_ENV === 'staging'){
+		await mongoose.connect(process.env.DB_HOST as string);
+		return;
+	};
+
+
 	let options: mongoose.ConnectOptions = { dbName: process.env.DB_NAME || 'df-test' };
 	const dbPath = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
 
-	if(process.env.NODE_ENV === 'production')
+	if(process.env.NODE_ENV === 'development')
 		options = {
 			...options,
 			user: process.env.DB_USER,
