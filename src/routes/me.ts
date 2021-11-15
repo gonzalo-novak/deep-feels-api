23a2fddeb8ea9de9808b2b59ab9c2cc027060f49
gonzalo-novak <gonzalo.novak@monstertrap.art>
2021-11-15 08:30:16 -0600

    chore: way of store profile photo in db has been changed

--- src/routes/me.ts
+++ src/routes/me.ts
@@ -1,10 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-
 import { Express } from 'express';
 import { authVerify } from '../middlewares/authVerify';
 import { User } from '../models/User';
 import { UploadedFile } from 'express-fileupload';
+import { v2 as cloudinary } from 'cloudinary';
 
 const me = (app: Express) => {
 	app.get('/user/:id/me', authVerify, async (req, res) => {
@@ -15,6 +13,12 @@ const me = (app: Express) => {
 	});
 
 	app.put('/user/:id/edit', authVerify, async (req, res) => {
+		cloudinary.config({ 
+			cloud_name: process.env.CLOUDINARY_NAME,
+			api_key: process.env.CLOUDINARY_API_KEY,
+			api_secret: process.env.CLOUDINARY_SECRET_KEY
+		});
+
 		const { id } = req.params;
 		const { email, name } = req.body as { email: string; name: string; };
 		const photo = req.files?.photo as UploadedFile;
@@ -37,17 +41,13 @@ const me = (app: Express) => {
 			if(photo) {
 				// Cleaning previously photo:
 				if(userDB.photo){
-					const currentPath = path.join(__dirname, '../uploads/', userDB.photo);
-					if(fs.existsSync(currentPath))
-						fs.unlinkSync(currentPath);
+					const splitName = userDB.photo.split('/');
+					const nameArr = splitName[splitName.length - 1];
+					const [ public_id ] = nameArr.split('.');
+					await cloudinary.uploader.destroy(public_id);
 				}
-
-				const tempName =
-					userDB._id + '.' + new Date().getTime() + '.' + photo.mimetype.split('/')[1];
-				const uploadPath = path.join(__dirname, '../uploads/', tempName);
-
-				await photo.mv(uploadPath);
-				userDB.photo = tempName;
+				const { secure_url } = await cloudinary.uploader.upload(photo.tempFilePath);
+				userDB.photo = secure_url;
 			}
 
 			// Saving changes
